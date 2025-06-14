3ce5288e0887c2035600ce0afe9db33cca3d6cf1
Timo Imhof <timo.imhof.uni@gmail.com>
2025-06-14 10:48:21 +0200

    BPE enhancement -- proper encoding and decoding: - Add mappings for encoding and decoding which are built while building the vocabulary - Simplify char lvl. encoding -> in turn simplifies handling end of sentences and end of sequences - make helper methods easier reusable by returning output

--- Transformer_Architecture/src/tokenizer/bpe.py
+++ Transformer_Architecture/src/tokenizer/bpe.py
@@ -3,22 +3,27 @@ from typing import Set, List, Tuple, Optional
 from collections import defaultdict
 from tqdm import tqdm
 import numpy as np
+from numpy.typing import NDArray
 
 
 @dataclass
 class BytePairEncoder_Config:
     vocab: Set[str] = field(default_factory=set)
     vocab_size: int = 37000
-    eow_token: str = "</w>"
+    end_of_word_token: str = "</w>"
+    out_of_vocab_idx: int = 0
 
 
 class BytePairEncoder:
     def __init__(self, config: BytePairEncoder_Config):
         self.config = config
+        # bpe vocabulary building
         self.vocab = config.vocab
         self.tokenized_corpus = []
         self.learned_merges = []
-        self.vocab_mapping = {}
+        # encoding and decoding mappings
+        self.mapping_token_to_idx = {}
+        self.mapping_idx_to_token = {}
 
     def _find_pairs(self) -> dict[Tuple, int]:
         pairs = defaultdict(int)
@@ -32,39 +37,45 @@ class BytePairEncoder:
             idx += 1
         return pairs
 
-    def _apply_merge(self, new_char: str) -> List[str]:
-        len_tokenized_corpus = len(self.tokenized_corpus)
+    def _apply_merge_to_input(self, new_char: str, input: list) -> List[str]:
+        len_input = len(input)
         idx = 0
-        while idx + 1 < len_tokenized_corpus:
-            candiate_merged_token = (
-                self.tokenized_corpus[idx] + self.tokenized_corpus[idx + 1]
-            )
+        while idx + 1 < len_input:
+            candiate_merged_token = input[idx] + input[idx + 1]
             if candiate_merged_token == new_char:
-                self.tokenized_corpus[idx] = candiate_merged_token
-                del self.tokenized_corpus[idx + 1]
-                len_tokenized_corpus -= 1
+                input[idx] = candiate_merged_token
+                del input[idx + 1]
+                len_input -= 1
             idx += 1
+        return input
 
     def _find_max_pair(self, pairs: dict[Tuple, int]) -> Optional[Tuple]:
         pairs_sorted_desc = sorted(pairs, key=lambda x: pairs[x], reverse=True)
         for pair in pairs_sorted_desc:
-            if self.config.eow_token in pair:
+            if self.config.end_of_word_token in pair:
                 continue
             return pair
         return
 
-    def _apply_char_lvl_encoding(self, text: str):
-        self.tokenized_corpus = []
-        for sentence in text.split("."):
-            for word in sentence.strip().split():
-                tokens = list(word) + [
-                    self.config.eow_token
-                ]  # Ensure tokens cannot be merged across words
-                self.tokenized_corpus.extend(tokens)
+    def _apply_char_lvl_encoding(self, text: str) -> list:
+        encoded_text = []
+        for char in text:
+            if char == " ":
+                encoded_text.append(self.config.end_of_word_token)
+            else:
+                encoded_text.append(char)
+        return encoded_text
 
     def build_vocabulary(self, corpus: str):
-        self._apply_char_lvl_encoding(corpus)
+        self.tokenized_corpus = self._apply_char_lvl_encoding(corpus)
         self.vocab = set(self.tokenized_corpus)
+
+        # Create mapping for base vocabulary
+        mapping_idx = 1
+        for tok in list(self.vocab):
+            self.mapping_idx_to_token[mapping_idx] = tok
+            self.mapping_token_to_idx[tok] = mapping_idx
+            mapping_idx += 1
 
         with tqdm(
             initial=len(self.vocab),
@@ -80,24 +91,38 @@ class BytePairEncoder:
                 if most_frequent_pair is None:
                     break
                 self.learned_merges.append(most_frequent_pair)
-                new_char = "".join(most_frequent_pair)
-                self.vocab.add(new_char)
-                self._apply_merge(new_char)
+                new_token = "".join(most_frequent_pair)
+                self.vocab.add(new_token)
+                self._apply_merge_to_input(new_token, self.tokenized_corpus)
+
+                # Extend mapping for merges
+                self.mapping_idx_to_token[mapping_idx] = new_token
+                self.mapping_token_to_idx[new_token] = mapping_idx
+                mapping_idx += 1
 
                 progress_bar.update(1)
 
-        for i, token in enumerate(self.vocab):
-            self.vocab_mapping[token] = i
-
-    def _convert_encoded_text_to_indices(self) -> np.ndarray:
-        numeric_repr = []
-        for token in self.tokenized_corpus:
-            numeric_repr.append(self.vocab_mapping.get(token, self.config.vocab_size))
-        return np.array(numeric_repr)
-
-    def encode(self, text: str) -> np.ndarray:
-        self._apply_char_lvl_encoding(text)
+    def encode(self, text: str) -> np.ndarray[int]:
+        char_lvl_encoded_text = self._apply_char_lvl_encoding(text)
         for merge in self.learned_merges:
             new_char = "".join(merge)
-            self._apply_merge(new_char)
-        return self._convert_encoded_text_to_indices()
+            char_lvl_encoded_text = self._apply_merge_to_input(
+                new_char, char_lvl_encoded_text
+            )
+
+        return np.array(
+            [
+                self.mapping_token_to_idx.get(tok, self.config.out_of_vocab_idx)
+                for tok in char_lvl_encoded_text
+            ]
+        )
+
+    def decode(self, input_ids: NDArray[np.int32]) -> str:
+        decoded_text = ""
+        for id in input_ids:
+            decoded_token = self.mapping_idx_to_token[id]
+            if decoded_token == self.config.end_of_word_token:
+                decoded_text += " "
+            else:
+                decoded_text += decoded_token
+        return decoded_text
